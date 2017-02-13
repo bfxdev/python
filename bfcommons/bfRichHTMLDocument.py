9cd86fc016a6aa8e327c739b26a78585643fbeec
blogoben <blogoben@users.noreply.github.com>
2017-02-13 06:20:04 +0100

    working version, SVG support started

--- bfcommons/bfRichHTMLDocument.py
+++ bfcommons/bfRichHTMLDocument.py
@@ -23,6 +23,27 @@ import bfElemTree as ET
 class RichHTMLDocument(ET.Element):
   """HTML Document with formatting and graphs"""
 
+  class Table: pass
+
+  class Graph:
+
+    def __init__(self, doc, title, attrs):
+      self.doc = doc
+      self.cursor = self.doc.add(tag="svg", html=title, attrs=attrs)
+      self.addRectangle(0,0,"100%","100%")
+
+
+    def addRectangle(self, x, y, w, h):
+      self.doc.saveCursor()
+      self.doc.setCursor(self.cursor)
+      self.doc.add(tag="rect", attrs=dict(x=x, y=y, width=w, height=h, style="stroke:black;fill:none;"))
+      self.doc.restoreCursor()
+
+
+  def addGraph(self, title, attrs=None):
+    return self.Graph(self, title, attrs)
+
+
   def __init__(self):
     """Inits an empty document"""
 
@@ -39,7 +60,7 @@ class RichHTMLDocument(ET.Element):
           th {background-color:lightgrey}
         </style>""")
 
-    self.add(tag="body", style="font-family:arial;")
+    self.add(tag="body", attrs=dict(style="font-family:arial;"))
 
     self.setCursor('body')
 
@@ -55,11 +76,20 @@ class RichHTMLDocument(ET.Element):
     ET.ElementTree(self).write(file, method='xml')
 
 
-  def setCursor(self, searchstring):
-    """Sets the cursor to an encapsulating element using searchstring. XML elements
-       are searched in the following order: str as id, then as xpath if id not found"""
+  def setCursor(self, obj):
+    """Sets the cursor to an encapsulating element using obj. XML elements
+       are searched in the following order:
+        - obj as Element
+        - obj as id string
+        - obj as xpath string if id not found"""
+
+    # First uses obj as Element
+    if hasattr(obj, 'findall'):
+      self.cursor = obj
+      return self.cursor
 
     # Searche Element using its id, if defined
+    searchstring = str(obj)
     res = self.find('.//*[@id="' + searchstring + '"]')
 
     # If not found, searches as XPath
@@ -84,38 +114,41 @@ class RichHTMLDocument(ET.Element):
     self.cursor = self.cursors.pop()
 
 
-  def add(self, html=None, text=None, tag=None, id=None, style=None):
+  def add(self, html=None, text=None, tag=None, attrs=None):
     """Adds data at cursor position, i.e. at the end of the pointed tag, depending on arguments:
-       - html: if defined, adds given data as text containing formatting tags ("<" and ">"
-          not escaped)
-       - text: if defined, adds given text as such, and escapes chars wherever necessary
-       - tag, id, style: if defined, encloses everything in a tag with id and style"""
+        - html: if defined, adds given data as text containing formatting tags ("<" and ">"
+           not escaped)
+        - text: if defined, adds given text as such, and escapes chars wherever necessary
+        - tag: if defined, encloses everything in a the given tag
+        - attrs: if tag and attrs defined, adds the given dict as attributes of the tag
+       Returns the newly created element if tage defined tag, otherwise the current cursor."""
 
     # If html is given, converts data to temporary element
     helem = None
     if html is not None and len(html) > 0:
       helem = ET.fromstring("<dummy>" + html + "</dummy>")
-      print ">>helem>>", ET.tostring(helem)
+      #print ">>helem>>", ET.tostring(helem)
 
 
     # If tag is defined, creates a new element with given attributes and retrieve data
     telem = None
     if tag is not None and len(tag)>0:
 
+      # Converts all given tags to strings
+      sattrs = {}
+      if attrs is not None:
+        for (k, v) in attrs.items():
+          sattrs[k] = str(v)
+
       # Creates tag with attributes
-      attrs = dict()
-      if id and len(id) > 0:
-        attrs['id'] = id
-      if style and len(style) > 0:
-        attrs['style'] = style
-      telem = ET.Element(tag, attrs)
+      telem = ET.Element(tag, sattrs)
 
       # Propagates data from helem if defined
       if helem is not None:
         telem.text = helem.text
         telem.tail = helem.tail     # should be None in all cases
         telem.extend(helem.findall("./*"))
-        print ">>telem>>", ET.tostring(telem)
+        #print ">>telem>>", ET.tostring(telem)
 
       # Adds the created tag at cursor position
       self.cursor.append(telem)
@@ -136,12 +169,14 @@ class RichHTMLDocument(ET.Element):
     if text is not None and len(text) > 0:
       ins = self.cursor if telem is None else telem
       l = ins.findall("./*")
-      print ">>cursor>>", self.cursor
-      print ">>findall>", l
+      #print ">>cursor>>", self.cursor
+      #print ">>findall>", l
       if len(l) == 0:
         ins.text = (ins.text if ins.text is not None else "") + text
       else:
         l[-1].tail = (l[-1].tail if l[-1].tail is not None else "") + text
+
+    return telem if telem is not None else self.cursor
 
 
   def show(self):
@@ -165,6 +200,9 @@ class RichHTMLDocument(ET.Element):
     app.exec_()
 
 
+
+
+
 def main(argv):
   """Auto-test"""
 
@@ -177,7 +215,7 @@ def main(argv):
   h = "First <em>heading</em> with formatting and id "
   t = "plus additional text with special characters & < >"
   print "Adding", h, "and", t
-  doc.add(tag="h1", html=h, text=t, id="head1")
+  doc.add(tag="h1", html=h, text=t, attrs=dict(id="head1"))
 
   h = "<p>first paragraph added as raw input with tag parameter</p>"
   print "Adding", h
@@ -185,7 +223,7 @@ def main(argv):
 
   h = "second paragraph added with tag and style"
   print "Adding", h
-  doc.add(tag="p", html=h, style="font-weight:bold;")
+  doc.add(tag="p", html=h, attrs=dict(style="font-weight:bold;"))
 
 
   h =  "Text with formatting <em>emphasis</em> and <b>bold</b> and newline.<br/>"
@@ -194,26 +232,31 @@ def main(argv):
   print "Adding without tag html =", h, "text =", t
   doc.add(html=h, text=t)
 
-  doc.add(tag="p", html="""
-  <svg height="150" width="500">
-  <defs>
-    <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-      <stop offset="0%" style="stop-color:rgb(255,255,255);stop-opacity:0" />
-      <stop offset="100%" style="stop-color:rgb(0,0,255);stop-opacity:1" />
-    </radialGradient>
-  </defs>
-  <ellipse cx="200" cy="70" rx="85" ry="55" fill="url(#grad1)" />
-  </svg>
-  """)
+  h = """<br/><form>Time Window from:<input type="datetime-local" name="from"/>
+         To:<input type="datetime-local" name="to"/></form>
+        <svg width="100%" height="500"> <rect width="100%" heigth="100%"/>
+        <ellipse cx="50%" cy="50%" rx="50%" ry="50%" /></svg>"""
+  print "Adding random html:", h
+  doc.add(html=h)
+
+  print "\n------------------- addGraph -----------------"
+  g = doc.addGraph("My Graph", attrs=dict(height="100%", width="100%"))
+  g.addRectangle(50, 50, 200, 200)
+
+  print "\n------------------- set/getCursor ----------------"
+  doc.saveCursor()
+  print "Current cursor value:", doc.getCursor()
+  print "Cursor at ID 'head1':", doc.setCursor('head1')
+  c = doc.getCursor()
+  print "Cursor at XPath 'body/form':", doc.setCursor('body/form')
+  print "Cursor back at head1 using Element:", doc.setCursor(c)
+  doc.restoreCursor()
+  print "Current cursor value after restoreCursor:", doc.getCursor()
 
 
   print "\n------------------- __str__ ----------------"
   print doc
 
-  print "\n------------------- setCursor ----------------"
-  doc.saveCursor()
-  print "Cursor at", doc.setCursor('head1')
-  doc.restoreCursor()
 
 
   print "\n------------------- write to test.html file ------------------"
